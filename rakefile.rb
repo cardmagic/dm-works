d60f13c3e930c4bdf14239b2138b204d400e252a
ssmoot <ssmoot@b9f95e1d-c398-435d-95f5-378b83d2b11d>
2007-06-08 03:20:33 +0000

    Reorganizing the project a bit more, filling out the Rake tasks.
    git-svn-id: http://datamapper.rubyforge.org/svn/trunk@110 b9f95e1d-c398-435d-95f5-378b83d2b11d

--- rakefile.rb
+++ rakefile.rb
@@ -2,6 +2,9 @@
 
 require 'rake'
 require 'spec/rake/spectask'
+require 'rake/rdoctask'
+require 'rake/gempackagetask'
+require 'rake/contrib/rubyforgepublisher'
 
 task :default => 'test'
 
@@ -19,4 +22,59 @@ end
 desc "Profile DataMapper"
 task :profile do
   load 'profile_data_mapper.rb'
+end
+
+PACKAGE_VERSION = '0.1.0'
+
+PACKAGE_FILES = FileList[
+  'README',
+  'CHANGELOG',
+  'MIT-LICENSE',
+  '*.rb',
+  'lib/**/*',
+  'spec/**/*'
+].to_a
+
+PROJECT = 'datamapper'
+
+desc "Generate Documentation"
+Rake::RDocTask.new do |rdoc|
+  rdoc.rdoc_dir = 'doc'
+  rdoc.title = "DataMapper -- An Object/Relational Mapper for Ruby"
+  rdoc.options << '--line-numbers' << '--inline-source' << '--main' << 'README'
+  rdoc.rdoc_files.include(PACKAGE_FILES)
+end
+
+gem_spec = Gem::Specification.new do |s| 
+  s.platform = Gem::Platform::RUBY 
+  s.name = PROJECT 
+  s.summary = "An Object/Relational Mapper for Ruby"
+  s.description = "It's ActiveRecord, but Faster, Better, Simpler."
+  s.version = PACKAGE_VERSION 
+   
+  s.authors = 'Sam Smoot'
+  s.email = 'ssmoot@gmail.com'
+  s.rubyforge_project = PROJECT 
+  s.homepage = 'http://datamapper.org' 
+   
+  s.files = PACKAGE_FILES 
+   
+  s.require_path = 'lib'
+  s.requirements << 'none'
+  s.autorequire = 'data_mapper'
+
+  s.has_rdoc = true 
+  s.rdoc_options << '--line-numbers' << '--inline-source' << '--main' << 'README' 
+  s.extra_rdoc_files = rd.rdoc_files.reject { |fn| fn =~ /\.rb$/ }.to_a 
+end
+
+Rake::GemPackageTask.new(gem_spec) do |p|
+  p.gem_spec = gem_spec
+  p.need_tar = true
+  p.need_zip = true
+end
+
+desc "Publish to RubyForge"
+task :rubyforge => [ :rdoc, :gem ] do
+  Rake::SshDirPublisher.new(ENV['RUBYFORGE_USER'], "/var/www/gforge-projects/#{PROJECT}", 'doc').upload
 end
