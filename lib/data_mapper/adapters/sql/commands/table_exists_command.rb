47cc76e8e94e3e6d2341c441fcbe72954d4fa281
ssmoot <ssmoot@b9f95e1d-c398-435d-95f5-378b83d2b11d>
2007-10-07 17:42:06 +0000

    Bugfix.
    git-svn-id: http://datamapper.rubyforge.org/svn/trunk@289 b9f95e1d-c398-435d-95f5-378b83d2b11d

--- lib/data_mapper/adapters/sql/commands/table_exists_command.rb
+++ lib/data_mapper/adapters/sql/commands/table_exists_command.rb
@@ -9,9 +9,13 @@ module DataMapper
             @adapter, @klass_or_name = adapter, klass_or_name
             @table = @adapter.table(@klass_or_name)
           end
-
+          
+          def table_name
+            @adapter.quote_value(@table.name)
+          end
+          
           def to_sql
-            "SHOW TABLES LIKE #{@adapter.quote_value(@table.name)}"
+            "SHOW TABLES LIKE #{table_name}"
           end
           
           def call
