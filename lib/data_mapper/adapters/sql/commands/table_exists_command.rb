e2c4987f6ba30c9bd637933847cfd00038e90115
jtoy <jtoy@b9f95e1d-c398-435d-95f5-378b83d2b11d>
2007-10-08 01:31:39 +0000

    the default behaviour is to use table_catalog when looking for a table, mysql's behaviour will be overwritten in its driver.
    git-svn-id: http://datamapper.rubyforge.org/svn/trunk@296 b9f95e1d-c398-435d-95f5-378b83d2b11d

--- lib/data_mapper/adapters/sql/commands/table_exists_command.rb
+++ lib/data_mapper/adapters/sql/commands/table_exists_command.rb
@@ -16,10 +16,10 @@ module DataMapper
           
           def to_sql
             unless table_name.index(".")
-              "select table_name from information_schema.tables where table_name = #{table_name} and table_schema = '#{@adapter.schema.name}'"
+              "select table_name from information_schema.tables where table_name = #{table_name} and table_catalog = '#{@adapter.schema.name}'"
             else
               table_schema, table_name = @table.name.split(".")
-              "select table_name from information_schema.tables where table_name = '#{table_name}' and table_schema = '#{table_schema}'"
+              "select table_name from information_schema.tables where table_name = '#{table_name}' and table_catalog = '#{table_schema}'"
             end
           end
           
