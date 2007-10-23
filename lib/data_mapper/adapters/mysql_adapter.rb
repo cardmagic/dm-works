bc4e14f2edf5e0ea8754df1a9f8d6dc3f37ba6d3
ssmoot <ssmoot@b9f95e1d-c398-435d-95f5-378b83d2b11d>
2007-10-23 00:59:10 +0000

    Changing default to UTF-8 for MysqlAdapter.
    git-svn-id: http://datamapper.rubyforge.org/svn/branches/do.rb@353 b9f95e1d-c398-435d-95f5-378b83d2b11d

--- lib/data_mapper/adapters/mysql_adapter.rb
+++ lib/data_mapper/adapters/mysql_adapter.rb
@@ -8,8 +8,20 @@ module DataMapper
       def create_connection
         conn = DataObject::Mysql::Connection.new("socket=/tmp/mysql.sock user=root dbname=data_mapper_1")
         conn.open
+        cmd = conn.create_command("SET NAMES UTF8")
+        cmd.execute_non_query
         return conn
       end
+      
+      module Mappings
+        
+        def to_create_table_sql
+          @to_create_table_sql || @to_create_table_sql = begin
+            "CREATE TABLE #{to_sql} (#{columns.map { |c| c.to_long_form }.join(', ')}) Type=MyISAM CHARACTER SET utf8"
+          end
+        end
+        
+      end # module Mappings
     end # class MysqlAdapter
     
   end # module Adapters
