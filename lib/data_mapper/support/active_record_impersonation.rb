82e80635b8bd224abd95237d341edf1173f36034
ssmoot <ssmoot@b9f95e1d-c398-435d-95f5-378b83d2b11d>
2007-10-27 21:41:40 +0000

    Resolves ticket#33
    git-svn-id: http://datamapper.rubyforge.org/svn/trunk@389 b9f95e1d-c398-435d-95f5-378b83d2b11d

--- lib/data_mapper/support/active_record_impersonation.rb
+++ lib/data_mapper/support/active_record_impersonation.rb
@@ -12,7 +12,7 @@ module DataMapper
       end
       
       def reload!
-        session.first(self.class, key, :select => session.mappings[self.class].columns.map(&:name), :reload => true)
+        session.first(self.class, key, :select => original_hashes.keys, :reload => true)
       end
       
       def reload
