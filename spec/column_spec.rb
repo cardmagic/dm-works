907d23636935f4037a8534a02c67f2d9f8105692
ssmoot <ssmoot@b9f95e1d-c398-435d-95f5-378b83d2b11d>
2007-10-27 04:25:06 +0000

    Forgot to add this earlier.
    git-svn-id: http://datamapper.rubyforge.org/svn/trunk@384 b9f95e1d-c398-435d-95f5-378b83d2b11d

---
+++ spec/column_spec.rb
@@ -0,0 +1,23 @@
+require File.dirname(__FILE__) + "/spec_helper"
+
+describe DataMapper::Adapters::Sql::Mappings::Column do
+  
+  it "should be unique within a set" do
+    
+    mappings = DataMapper::Adapters::Sql::Mappings
+    
+    columns = SortedSet.new
+
+    columns << mappings::Column.new(database(:mock).adapter, nil, :one, :string, 1)
+    columns << mappings::Column.new(database(:mock).adapter, nil, :two, :string, 2)
+    columns << mappings::Column.new(database(:mock).adapter, nil, :three, :string, 3)
+    columns.should have(3).entries
+    
+    columns << mappings::Column.new(database(:mock).adapter, nil, :two, :integer, 3)
+    columns.should have(3).entries
+    
+    columns << mappings::Column.new(database(:mock).adapter, nil, :id, :integer, -1)
+    columns.should have(4).entries
+  end
+  
+end
