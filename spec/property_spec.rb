4cbd30696d16fb581ecc7c53c3ea38c230440b31
ssmoot <ssmoot@b9f95e1d-c398-435d-95f5-378b83d2b11d>
2007-11-10 01:41:02 +0000

    Added more specs for properties.
    git-svn-id: http://datamapper.rubyforge.org/svn/trunk@470 b9f95e1d-c398-435d-95f5-378b83d2b11d

--- spec/property_spec.rb
+++ spec/property_spec.rb
@@ -20,3 +20,47 @@ describe DataMapper::Property do
   
   it "should add a validates_format_of if you pass a format option"
 end
+
+describe DataMapper::Adapters::Sql::Mappings do
+  
+  it "should return the same Table instance for two objects mapped to the same database table" do
+    # Refers to the same Table instance
+    database.table(Person) == database.table(SalesPerson)
+  end
+  
+  it "should have one super-set of total mapped columns" do
+    # Refers to the mapped columns
+    database.table(Person).columns == database.table(SalesPerson).columns
+  end
+  
+  it "should have one set of columns that represents the actual database" do
+    # Refers to the actual columns in the database, which may/are-likely-to-be different
+    # than the mapped columns, sometimes just because your models are dealing with
+    # a legacy database where not every column is mapped to the new model, so this
+    # is expected.
+    database.table(Person).send(:database_columns) == database.table(SalesPerson).send(:database_columns)
+  end
+  
+  it "should have two different sets of mapped properties that point to subsets of the Table columns" do
+    table = database.table(Person)
+    
+    # Every property's column should be represented in the Table's column mappings.
+    Person.properties.each do |property|
+      table.columns.should include(property.column)
+    end
+    
+    # For both models in the STI setup...
+    SalesPerson.properties.each do |property|
+      table.columns.should include(property.column)
+    end
+    
+    # Even though Person's properties are fewer than a SalesPerson's
+    Person.properties.size.should_not eql(SalesPerson.properties.size)
+    
+    # And Person's properties should be a subset of a SalesPerson's
+    Person.properties.each do |property|
+      SalesPerson.properties.map(&:column).should include(property.column)
+    end
+  end
+  
+end
