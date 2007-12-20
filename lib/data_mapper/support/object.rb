64843ea019cbc09958d53304053868f99254a306
ssmoot <ssmoot@b9f95e1d-c398-435d-95f5-378b83d2b11d>
2007-12-20 22:43:51 +0000

    Just some benches I ran.
    git-svn-id: http://datamapper.rubyforge.org/svn/trunk@624 b9f95e1d-c398-435d-95f5-378b83d2b11d

--- lib/data_mapper/support/object.rb
+++ lib/data_mapper/support/object.rb
@@ -28,3 +28,27 @@ end
 class Object #:nodoc:
   include DataMapper::Support::Object
 end
+
+# require 'benchmark'
+# 
+# N = 1_000_000
+# 
+# puts Benchmark.measure {
+#   N.times { Object.recursive_const_get('DataMapper::Support::Object') }
+# }
+# 
+# puts Benchmark.measure {
+#   N.times {
+#     klass = Object
+#     'DataMapper::Support::Object'.split('::').each do |c|
+#       klass = klass.const_get(c)
+#     end
+#     klass  
+#   }
+# }
+# 
+# __END__
+# >>> object.rb
+# 
+#   0.910000   0.000000   0.910000 (  0.916914)
+#   6.140000   0.010000   6.150000 (  6.151984)
