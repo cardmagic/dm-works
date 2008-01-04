25ca9f98826315f8532d730b46afb27a89bea4f5
lanaer <lanaer@b9f95e1d-c398-435d-95f5-378b83d2b11d>
2008-01-04 22:30:29 +0000

    Added is_a_tree functionality
    git-svn-id: http://datamapper.rubyforge.org/svn/trunk@678 b9f95e1d-c398-435d-95f5-378b83d2b11d

--- lib/data_mapper/base.rb
+++ lib/data_mapper/base.rb
@@ -1,8 +1,10 @@
 require 'data_mapper/persistence'
+require 'data_mapper/is/tree'
 
 module DataMapper
 
   class Base
+		include DataMapper::Is::Tree
     
     def self.inherited(klass)
       klass.send(:include, DataMapper::Persistence)
