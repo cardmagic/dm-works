22dd3ef2a7f98f7312b07f3b33c62a6e56611f14
bernerdschaefer <bernerdschaefer@b9f95e1d-c398-435d-95f5-378b83d2b11d>
2008-01-06 16:32:46 +0000

    Moved methods in AR Impersonation to DM::Persistence::ConvenienceMethods (http://wm.lighthouseapp.com/projects/4819/tickets/76)
    git-svn-id: http://datamapper.rubyforge.org/svn/trunk@685 b9f95e1d-c398-435d-95f5-378b83d2b11d

--- lib/data_mapper/persistence.rb
+++ lib/data_mapper/persistence.rb
@@ -1,6 +1,5 @@
 require 'data_mapper/property'
 require 'data_mapper/attributes'
-require 'data_mapper/support/active_record_impersonation'
 require 'data_mapper/support/serialization'
 require 'data_mapper/validations'
 require 'data_mapper/associations'
@@ -21,12 +20,13 @@ module DataMapper
     def self.included(klass)
       
       klass.extend(ClassMethods)
+      klass.extend(ConvenienceMethods::ClassMethods)
       
+      klass.send(:include, ConvenienceMethods::InstanceMethods)
       klass.send(:include, Attributes)
       klass.send(:include, Associations)
       klass.send(:include, Validations)
       klass.send(:include, CallbacksHelper)
-      klass.send(:include, Support::ActiveRecordImpersonation)
       klass.send(:include, Support::Serialization)
 
       klass.instance_variable_set('@properties', [])
@@ -88,6 +88,108 @@ module DataMapper
         when Hash then self.attributes = details
         when details.respond_to?(:persistent?) then self.private_attributes = details.attributes
         when Struct then self.private_attributes = details.attributes
+        end
+      end
+    end
+    
+    module ConvenienceMethods
+      module InstanceMethods
+        def save
+          database_context.save(self)
+        end
+
+        def save!
+          raise ValidationError.new(errors) unless save
+          return true
+        end
+
+        def reload!(cols = nil)
+          database_context.first(self.class, key, :select => ([self.class.table.key.to_sym] + (cols || original_values.keys)).uniq, :reload => true)
+          self.loaded_associations.each { |association| association.reload! }
+          self
+        end
+        alias reload reload!
+
+        def destroy!
+          database_context.destroy(self)
+        end
+      end
+        
+      module ClassMethods
+        def find_or_create(search_attributes, create_attributes = {})
+          first(search_attributes) || create(search_attributes.merge(create_attributes))
+        end
+      
+        def all(options = {})
+          database.all(self, options)
+        end
+      
+        def each(options = {}, &b)
+          raise ArgumentError.new(":offset is not supported with the #each method") if options.has_key?(:offset)
+
+          offset = 0
+          limit = options[:limit] || (self::const_defined?('DEFAULT_LIMIT') ? self::DEFAULT_LIMIT : 500)
+
+          until (results = all(options.merge(:limit => limit, :offset => offset))).empty?
+            results.each(&b)
+            offset += limit
+          end
+        end
+      
+        def first(*args)
+          database.first(self, *args)
+        end
+      
+        def count(*args)
+          database.count(self, *args)
+        end
+      
+        def delete_all
+          database.delete_all(self)
+        end
+      
+        def truncate!
+          database.truncate(self)
+        end
+      
+        def find(type_or_id, options = {})
+          case type_or_id
+            when :first then first(options)
+            when :all then all(options)
+            else first(type_or_id, options)
+          end
+        end
+      
+        def find_by_sql(*args)
+          DataMapper::database.query(*args)
+        end
+      
+        def get(*keys)
+          database.get(self, keys)
+        end
+      
+        def [](*keys)
+          # Eventually this ArgumentError should be removed. It's only here to help
+          # migrate users away from the [options_hash] syntax, which is no longer supported.
+          raise ArgumentError.new('Hash is not a valid key') if keys.size == 1 && keys.first.is_a?(Hash)
+          instance = database.get(self, keys)
+          raise ObjectNotFoundError.new() unless instance
+          return instance
+        end
+      
+        def create(attributes)
+          instance = self.new
+          instance.attributes = attributes
+          instance.save
+          instance
+        end
+      
+        def create!(attributes)
+          instance = self.new
+          instance.attributes = attributes
+          instance.save
+          raise ObjectNotFoundError.new(instance) if instance.new_record?
+          instance
         end
       end
     end
