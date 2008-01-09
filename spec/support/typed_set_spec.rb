f58c642f4ad7905123a53f0efa9de940e0c70428
ssmoot <ssmoot@b9f95e1d-c398-435d-95f5-378b83d2b11d>
2008-01-09 22:58:59 +0000

    Fix to not require fixtures loaded.
    git-svn-id: http://datamapper.rubyforge.org/svn/trunk@696 b9f95e1d-c398-435d-95f5-378b83d2b11d

--- spec/support/typed_set_spec.rb
+++ spec/support/typed_set_spec.rb
@@ -1,23 +1,23 @@
 require File.dirname(__FILE__) + "/../spec_helper"
 
 describe DataMapper::Support::TypedSet do
-
+  
   it "should accept objects of a defined type, and raise for others" do
 
     s = DataMapper::Support::TypedSet.new(Zoo, Animal)
 
     lambda do
-      s << Zoo.first
+      s << Zoo.new
       s.size.should == 1
     end.should_not raise_error(ArgumentError)
 
     lambda do
-      s << Animal.first
+      s << Animal.new
       s.size.should == 2
     end.should_not raise_error(ArgumentError)
 
     lambda do
-      s << Exhibit.first
+      s << Exhibit.new
       s.size.should == 2
     end.should raise_error(ArgumentError)
 
